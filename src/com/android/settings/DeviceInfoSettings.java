04d472d9031093bb5476c99eb66f92e1c9c05cc6
aow1980 <wilkes.allen@gmail.com>
2015-04-25 22:57:42 -0400

    Twisted Build Date
    * To visably show the day ROM was built

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -79,6 +79,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_MOD_BUILD_DATE = "build_date";
     private static final String KEY_UBER_AND = "uber_android";
     private static final String KEY_UBER_KERNEL = "uber_kernel";
     private static final String KEY_UBER_FLAGS = "uber_flags";
@@ -107,6 +108,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         findPreference(KEY_KERNEL_VERSION).setEnabled(true);
         findPreference(KEY_UBER_KERNEL).setEnabled(true);
         findPreference(KEY_UBER_FLAGS).setEnabled(true);
+        setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
         setValueSummary(KEY_UBER_AND, "ro.uber.android");
         setValueSummary(KEY_UBER_KERNEL, "ro.uber.kernel");
         setValueSummary(KEY_UBER_FLAGS, "ro.uber.flags");
