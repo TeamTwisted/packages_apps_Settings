fff89eec5fe16d19b581d9346aaaf918f62e0cf2
aow1980 <wilkes.allen@gmail.com>
2015-05-10 16:48:30 -0400

    Customizeable QS Tiles (2/2) Squashed
    Thanks to CyanogenMod, Euphoria OS and all the others who contributed!
    
    Add customization for Quick Settings [2/2]
    
    Add a new section to customize Quick Settings.
    
    Modified DraggableGridView to arrange the items differently.
    
    Gave everything a "Material-ish" style.
    
    Inversion and Hotspot tiles can be rearranged now.
    
    Creadted a "Notification Drawer" category.
    
    QS: change location sensors mode in quick settings [2/2]
    Added switch in notification drawer settings
    to enable advanced setting for location tile
    
    add missing string for Rotation tile
    
    add data and notification tile
    
    remove notification tile
    
    QS: Longclick action [2/2]
    Long press expanded status bar Settings button opens tiles settings
    
    Thanks to @romanbb for status bar weather code & @Altaf-Mahdi for Status Bar Header haptic feedback option
    
    Signed-off-by: snak3ater <saeedyasir91@gmail.com>
    
    Cleanup and fix QS tile implementation
    
    - Decouple DraggableGridView and QSTiles code by shifting around some
      logic
    - Fix column-for-position calculation
    - Improve layouts and preference ordering
    
    Change-Id: I7a06150f2602888b0f28fca93e0164e381272b67
    
    QS: some corrections on location tile code (2/2)
    
    Setting moved to Secure
    
    Wifi: add option to show quick settings detail view (2/2)
    
    Added a switch preference in notification drawer settings
    to enable detail view for Wi-Fi tile
    
    Settings : Sort QSTiles in picker
    
    Change-Id: I677226a456ee8673860d4de291ba435d3ad00cbb
    
    switch to SecureSwitchpreference
    
    Settings : Update for centralized quick settings
    
    Change-Id: If9cad4fd45e5bf7deb13aa1a5284ac5566e44a95
    
    Cleanup QS.
    
    Settings: add newly added toggles
    
    Change-Id: I338eae4546bbc6c9f8453141bf2a8e5afb41fa7b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Properly name string resources.
    
    Remove Lockscreen tile. Nobody needs that
    
    QS: add Data tile and cleanup
    
    U nasty little bastard ....
    damn this one sucks my nerves
    Fix QS Tiles
    
    Quick Settings: Auto collapse panel (2/2)
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Change-Id: I7052a2df7e92d2828769d5a8d3230ba47b11faa9
    
    QS: Option to show four tiles per row (2/2)
    
    This also changes tile picker grid to 4 tiles per row
    
    Settings clean up
    
    Reorganize notification drawer settings
    
    Quick settings: Option to disable brightness slider (2/2)
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    [2/2] Settings: Add Haptic Feedback to quicktiles
    
    QS: add newly added tiles
    
    * Battery saver
    * Brightness
    * Expanded desktop
    
    Change-Id: I64956333bd22d26db6ce72a6149b496d4542bfec
    
    Fixes for 5.1 implementation
    
    (cherry picked from commit d863cbfd63be40e93193830aecebaa6c36ec2f13)

---
+++ src/com/android/settings/terminus/qs/QSTileHolder.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.terminus.qs;
+
+import android.content.Context;
+
+import com.android.internal.util.cm.QSConstants;
+import com.android.internal.util.cm.QSUtils;
+import com.android.settings.R;
+
+/**
+ * This class holds the icon, the name - or the string the user sees,
+ * and the value which will be stored
+ */
+public class QSTileHolder {
+    public static final String TILE_ADD_DELETE = "";
+
+    public final String resourceName;
+    public final String value;
+    public final String name;
+
+    public QSTileHolder(String resourceName, String value, String name) {
+        this.resourceName = resourceName;
+        this.value = value;
+        this.name = name;
+    }
+
+    public static QSTileHolder from(Context context, String tileType) {
+        String resourceName = null;
+        int stringId = -1;
+
+        if (!TILE_ADD_DELETE.equals(tileType) &&
+                !QSUtils.getAvailableTiles(context).contains(tileType)) {
+            return null;
+        }
+
+        switch (tileType) {
+            case TILE_ADD_DELETE:
+                break;
+            case QSConstants.TILE_WIFI:
+                resourceName = "ic_qs_wifi_full_4";
+                stringId = R.string.qs_tile_wifi;
+                break;
+            case QSConstants.TILE_BLUETOOTH:
+                resourceName = "ic_qs_bluetooth_on";
+                stringId = R.string.qs_tile_bluetooth;
+                break;
+            case QSConstants.TILE_INVERSION:
+                resourceName = "ic_qs_inversion_on";
+                stringId = R.string.qs_tile_display_inversion;
+                break;
+            case QSConstants.TILE_CELLULAR:
+                resourceName = "ic_qs_signal_full_4";
+                stringId = R.string.qs_tile_cellular_data;
+                break;
+            case QSConstants.TILE_AIRPLANE:
+                resourceName = "ic_qs_airplane_on";
+                stringId = R.string.qs_tile_airplane_mode;
+                break;
+            case QSConstants.TILE_ROTATION:
+                resourceName = "ic_qs_rotation_locked";
+                stringId = R.string.qs_tile_display_rotation;
+                break;
+            case QSConstants.TILE_FLASHLIGHT:
+                resourceName = "ic_qs_flashlight_on";
+                stringId = R.string.qs_tile_flashlight;
+                break;
+            case QSConstants.TILE_LOCATION:
+                resourceName = "ic_qs_location_on";
+                stringId = R.string.qs_tile_location;
+                break;
+            case QSConstants.TILE_CAST:
+                resourceName = "ic_qs_cast_on";
+                stringId = R.string.qs_tile_cast_screen;
+                break;
+            case QSConstants.TILE_HOTSPOT:
+                resourceName = "ic_qs_hotspot_on";
+                stringId = R.string.qs_tile_hotspot;
+                break;
+            case QSConstants.TILE_NOTIFICATIONS:
+                resourceName = "ic_qs_ringer_audible";
+                stringId = R.string.qs_tile_notifications;
+                break;
+            case QSConstants.TILE_NFC:
+                resourceName = "ic_qs_nfc_on";
+                stringId = R.string.qs_tile_nfc;
+                break;
+            case QSConstants.TILE_COMPASS:
+                resourceName = "ic_qs_compass_on";
+                stringId = R.string.qs_tile_compass;
+                break;
+            case QSConstants.TILE_DATA:
+                resourceName = "ic_qs_data_on";
+                stringId = R.string.qs_tile_data;
+                break;
+            case QSConstants.TILE_BATTERY_SAVER:
+                resourceName = "ic_qs_battery_saver_on";
+                stringId = R.string.qs_battery_saver_tile;
+                break;
+            case QSConstants.TILE_BRIGHTNESS:
+                resourceName = "ic_qs_brightness_auto_off_alpha";
+                stringId = R.string.qs_brightness_tile;
+                break;
+            case QSConstants.TILE_EXPANDED_DESKTOP:
+                resourceName = "ic_qs_expanded_desktop";
+                stringId = R.string.qs_expanded_desktop_tile;
+                break;
+            default:
+                return null;
+        }
+
+        String name = stringId != -1 ? context.getString(stringId) : null;
+        return new QSTileHolder(resourceName, tileType, name);
+    }
+}
