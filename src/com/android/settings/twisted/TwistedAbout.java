d5d382f6a3bf3dd8b6b5a7cb92b7a6dff2a55c7d
xWASABI <wasabi.dev@gmail.com>
2015-10-13 09:46:32 -0500

    Twisted meets Android m6.0 ** Initial commit

---
+++ src/com/android/settings/twisted/TwistedAbout.java
@@ -0,0 +1,23 @@
+package com.android.settings.twisted;
+
+import com.android.internal.logging.MetricsLogger;
+
+import android.os.Bundle;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class TwistedAbout extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.twisted_about);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DISPLAY;
+    }
+}
---
+++ src/com/android/settings/twisted/TwistedApps.java
@@ -0,0 +1,23 @@
+package com.android.settings.twisted;
+
+import com.android.internal.logging.MetricsLogger;
+
+import android.os.Bundle;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class TwistedApps extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.twisted_apps);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DISPLAY;
+    }
+}
