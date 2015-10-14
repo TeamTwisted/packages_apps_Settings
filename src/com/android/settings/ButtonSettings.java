15e46f3f169b927e63dce37fd742a22d1af973c3
zephiK <zephik@gmail.com>
2015-10-14 17:36:22 -0400

    Volume Rocker Wake [2/2]
    Based on Rascarlo's code - https://github.com/rascarlo/android_packages_apps_settings/commit/33af4f0d419533bcb62ae16ea46bf3c134bf12ca
    
    Change-Id: Idbc643ae3e55777f8eeb0b1c82b9a09dd4ba7998

---
+++ src/com/android/settings/ButtonSettings.java
@@ -0,0 +1,71 @@
+/*
+* Copyright (C) 2015 Benzo Rom
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.settings;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.internal.logging.MetricsLogger;
+
+public class ButtonSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = ButtonSettings.class.getSimpleName();
+
+    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
+    private SwitchPreference mVolumeRockerWake;
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DEVELOPMENT;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.button_settings);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        // volume rocker wake
+        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
+        mVolumeRockerWake.setOnPreferenceChangeListener(this);
+        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_WAKE, 0);
+        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mVolumeRockerWake) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
+                    value ? 1 : 0);
+       }
+        return true;
+    }
+}
