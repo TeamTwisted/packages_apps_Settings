986f26f8266a11d85feadf79eb327e5161a5f749
Lennart <lennart.1997@gmx.de>
2015-12-11 22:52:53 +0100

    Update

--- src/com/android/settings/twisted/StatusBarSettings.java
+++ src/com/android/settings/twisted/StatusBarSettings.java
@@ -1,30 +1,18 @@
 package com.android.settings.twisted;
 
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 
-import com.android.internal.logging.MetricsLogger;
-
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
+import com.android.internal.logging.MetricsLogger;
 
-public class StatusBarSettings extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
+public class StatusBarSettings extends SettingsPreferenceFragment {
 
-    private static final String PREF_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
-
-    private SwitchPreference mCustomHeader;
-
-    @Override
+   @Override
     protected int getMetricsCategory() {
         return MetricsLogger.DEVELOPMENT;
     }
@@ -34,38 +22,4 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.twisted_statusbar);
     }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
-
-        // Status bar custom header hd
-        mCustomHeader = (SwitchPreference) findPreference(PREF_CUSTOM_HEADER_DEFAULT);
-        mCustomHeader.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, 0) == 1));
-        mCustomHeader.setOnPreferenceChangeListener(this);
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getContentResolver();
-         if (preference == mCustomHeader) {
-           boolean value = (Boolean) newValue;
-           Settings.System.putInt(resolver,
-                   Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, value ? 1 : 0);
-            return true;
-        }
-        return false;
-    }
-
 }
-
