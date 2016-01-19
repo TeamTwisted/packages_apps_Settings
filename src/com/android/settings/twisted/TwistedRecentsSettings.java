eeb66e390142416bde278db2f5025e2484c6c771
Alex Cruz <mazdarider23@gmail.com>
2016-01-18 19:24:40 -0500

    Immersive Recents [2/2]
    This adds on to previous mod called FULL SCREEN RECENTS but now adds options
    to show recents with statusbar only or navbar only as well as fullscreen.
    
    - Renamed the mod to immersive recents to avoid confusion moving forward
    
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only

--- src/com/android/settings/twisted/TwistedRecentsSettings.java
+++ src/com/android/settings/twisted/TwistedRecentsSettings.java
@@ -1,12 +1,18 @@
 package com.android.settings.twisted;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
-
+import android.preference.SwitchPreference;
+import android.provider.Settings;
 import com.android.internal.logging.MetricsLogger;
 
 import com.android.settings.R;
@@ -14,7 +20,26 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import java.util.List;
 
-public class TwistedRecentsSettings extends SettingsPreferenceFragment {
+public class TwistedRecentsSettings extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
+
+    private static final String IMMERSIVE_RECENTS = "immersive_recents";
+
+    private ListPreference mImmersiveRecents;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.twisted_recents);
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS);
+        mImmersiveRecents.setValue(String.valueOf(Settings.System.getInt(
+               getContentResolver(), Settings.System.IMMERSIVE_RECENTS, 0)));
+        mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+        mImmersiveRecents.setOnPreferenceChangeListener(this);
+    }
 
     @Override
     protected int getMetricsCategory() {
@@ -22,10 +47,15 @@ public class TwistedRecentsSettings extends SettingsPreferenceFragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.twisted_recents);
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mImmersiveRecents) {
+            Settings.System.putInt(getContentResolver(),
+            Settings.System.IMMERSIVE_RECENTS, Integer.valueOf((String) objValue));
+            mImmersiveRecents.setValue(String.valueOf(objValue));
+            mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+        }
+        return false;
     }
 }
 
