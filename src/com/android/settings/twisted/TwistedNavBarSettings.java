aa3f6940dff337e8a0432d2d17758d743e8e6f9f
Lennart <lennart.1997@gmx.de>
2016-01-27 11:51:04 +0100

    Twisted Update * fixed the slim navabr * added a new screen for the slim menu options * added dependencies to slim navbar * added back cursor buttons * corrected some strings

--- src/com/android/settings/twisted/TwistedNavBarSettings.java
+++ src/com/android/settings/twisted/TwistedNavBarSettings.java
@@ -19,23 +19,19 @@ public class TwistedNavBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "NavBar";
-    private static final String PREF_MENU_LOCATION = "pref_navbar_menu_location";
-    private static final String PREF_NAVBAR_MENU_DISPLAY = "pref_navbar_menu_display";
     private static final String ENABLE_NAVIGATION_BAR = "enable_nav_bar";
     private static final String PREF_BUTTON = "navbar_button_settings";
-    private static final String PREF_BUTTON_STYLE = "nav_bar_button_style";
     private static final String PREF_STYLE_DIMEN = "navbar_style_dimen_settings";
     private static final String PREF_NAVIGATION_BAR_CAN_MOVE = "navbar_can_move";
+    private static final String STATUS_BAR_IME_ARROWS = "status_bar_ime_arrows";
 
     private int mNavBarMenuDisplayValue;
 
-    ListPreference mMenuDisplayLocation;
-    ListPreference mNavBarMenuDisplay;
     SwitchPreference mEnableNavigationBar;
     SwitchPreference mNavigationBarCanMove;
     PreferenceScreen mButtonPreference;
-    PreferenceScreen mButtonStylePreference;
     PreferenceScreen mStyleDimenPreference;
+    SwitchPreference mStatusBarImeArrows;
 
     @Override
     protected int getMetricsCategory() {
@@ -52,21 +48,7 @@ public class TwistedNavBarSettings extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefs = getPreferenceScreen();
 
-        mMenuDisplayLocation = (ListPreference) findPreference(PREF_MENU_LOCATION);
-        mMenuDisplayLocation.setValue(Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.MENU_LOCATION,
-                0) + "");
-        mMenuDisplayLocation.setOnPreferenceChangeListener(this);
-
-        mNavBarMenuDisplay = (ListPreference) findPreference(PREF_NAVBAR_MENU_DISPLAY);
-        mNavBarMenuDisplayValue = Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.MENU_VISIBILITY,
-                2);
-        mNavBarMenuDisplay.setValue(mNavBarMenuDisplayValue + "");
-        mNavBarMenuDisplay.setOnPreferenceChangeListener(this);
-
         mButtonPreference = (PreferenceScreen) findPreference(PREF_BUTTON);
-        mButtonStylePreference = (PreferenceScreen) findPreference(PREF_BUTTON_STYLE);
         mStyleDimenPreference = (PreferenceScreen) findPreference(PREF_STYLE_DIMEN);
 
         // A Nexus will always have a NavBar by default
@@ -81,32 +63,24 @@ public class TwistedNavBarSettings extends SettingsPreferenceFragment implements
                 DeviceUtils.isPhone(getActivity()) ? 1 : 0) == 0);
         mNavigationBarCanMove.setOnPreferenceChangeListener(this);
 
+        mStatusBarImeArrows = (SwitchPreference) findPreference(STATUS_BAR_IME_ARROWS);
+        mStatusBarImeArrows.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_IME_ARROWS, 0) == 1);
+        mStatusBarImeArrows.setOnPreferenceChangeListener(this);
+
         updateNavbarPreferences(enableNavigationBar);
     }
 
     private void updateNavbarPreferences(boolean show) {
-        mNavBarMenuDisplay.setEnabled(show);
         mButtonPreference.setEnabled(show);
-        mButtonStylePreference.setEnabled(show);
         mStyleDimenPreference.setEnabled(show);
         mNavigationBarCanMove.setEnabled(show);
-        mMenuDisplayLocation.setEnabled(show
-            && mNavBarMenuDisplayValue != 1);
+       mStatusBarImeArrows.setEnabled(show);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mMenuDisplayLocation) {
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.MENU_LOCATION, Integer.parseInt((String) newValue));
-            return true;
-        } else if (preference == mNavBarMenuDisplay) {
-            mNavBarMenuDisplayValue = Integer.parseInt((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.MENU_VISIBILITY, mNavBarMenuDisplayValue);
-            mMenuDisplayLocation.setEnabled(mNavBarMenuDisplayValue != 1);
-            return true;
-        } else if (preference == mEnableNavigationBar) {
+        if (preference == mEnableNavigationBar) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_SHOW,
                     ((Boolean) newValue) ? 1 : 0);
@@ -116,6 +90,11 @@ public class TwistedNavBarSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_CAN_MOVE,
                     ((Boolean) newValue) ? 0 : 1);
+            return true;
+        } else if (preference == mStatusBarImeArrows) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_IME_ARROWS,
+                    ((Boolean) newValue) ? 1 : 0);
             return true;
         }
         return false;
