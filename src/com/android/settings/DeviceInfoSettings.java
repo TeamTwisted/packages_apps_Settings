bf060f58a36a1c330343721c7b43e0b556048647
kantjer <kantjer@gmail.com>
2016-03-10 09:32:10 +0100

    Remove "Gcc android version" from "About phone" screen

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -80,7 +80,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_TWISTED_VERSION = "twisted_version";
-    private static final String KEY_SM_AND = "sm_android";
     private static final String KEY_SM_FLAGS = "sm_flags";
     private static final String KEY_DTC_VERSION = "dtc_version";
     private static final String PROPERTY_DTC_VERSION = "ro.dtc.version";
@@ -133,9 +132,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
         setValueSummary(KEY_TWISTED_VERSION, "ro.twisted.version");
-        findPreference(KEY_SM_AND).setEnabled(true);
         findPreference(KEY_SM_FLAGS).setEnabled(true);
-        setValueSummary(KEY_SM_AND, "ro.sm.android");
         setValueSummary(KEY_SM_FLAGS, "ro.sm.flags");
         setValueSummary(KEY_DTC_VERSION, "ro.dtc.version");
 
